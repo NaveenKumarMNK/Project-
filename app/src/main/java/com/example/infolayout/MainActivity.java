738d4fd7e46a25fbd7ca315668e3acd7738b68bf
naveen <naveenmk675@gmail.com>
2021-03-02 22:42:24 +0530

    First Commit

---
+++ app/src/main/java/com/example/infolayout/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.infolayout;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    Toolbar toolbar;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        toolbar=findViewById(R.id.Toolbar);
+        setSupportActionBar(toolbar);
+        if (getSupportActionBar()!=null){
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        }
+    }
+}
